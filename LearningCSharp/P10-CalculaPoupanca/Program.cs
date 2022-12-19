1919a3b2ef8278965dc0ec2539475561c4897414
Brenno Graça <brennosg_3712@hotmail.com>
2022-12-19 09:20:36 -0300

    Loop control

---
+++ LearningCSharp/P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 10 - Calcula poupança");
+
+            double valorInvestido = 1000;
+
+            //0.36% = 0.0036
+            /*
+            valorInvestido = valorInvestido + valorInvestido * 0.0036;
+            Console.WriteLine("Após 1 mês, você terá R$" + valorInvestido);
+
+            valorInvestido = valorInvestido + valorInvestido * 0.0036;
+            Console.WriteLine("Após 2 meses, você terá R$" + valorInvestido);
+            */
+
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após {0} meses, você terá R$ {1}", contadorMes, valorInvestido);
+
+                contadorMes++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
