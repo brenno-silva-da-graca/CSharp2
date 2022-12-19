d0c55ee19aec5a2a84b5f7c27003b02057b28cf9
Brenno Graça <brennosg_3712@hotmail.com>
2022-12-19 10:14:44 -0300

    First class

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titular = "Gabriela";
+            int numeroAgencia = 863;
+            int numeroConta = 863146;
+            double saldo = 100;
+
+            string titular2 = "Bruno";
+            int numeroAgencia2 = 863;
+            int numeroConta2 = 863146;
+            double saldo2 = 100;
+
+            new ContaCorrente();
+
+            Console.ReadLine();
+        }
+    }
+}
