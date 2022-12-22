dd7c21f6036a26ab3644bce843089be224cf8379
Brenno Graça <brennosg_3712@hotmail.com>
2022-12-22 12:00:28 -0300

    Class 3: Director class and Overloads

--- ByteBank2/ByteBank/Funcionarios/Diretor.cs
+++ ByteBank2/ByteBank/Funcionarios/Diretor.cs
@@ -8,6 +8,13 @@ namespace ByteBank.Funcionarios
 {
     public class Diretor
     {
-        
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public double GetBonificacao()
+        {
+            return Salario;
+        }
     }
 }
--- ByteBank2/ByteBank/Funcionarios/Funcionario.cs
+++ ByteBank2/ByteBank/Funcionarios/Funcionario.cs
@@ -8,28 +8,12 @@ namespace ByteBank.Funcionarios
 {
     public class Funcionario
     {
-        //0 - Funcionário
-        //1 - Diretor
-        //2 - Designer
-        //3 - Gerente de conta corrente
-        //4 - Coordenador
-        //N - ...
-        private int _tipo;
-
         public string Nome { get; set; }
         public string CPF { get; set; }
         public double Salario { get; set; }
-
-        public Funcionario(int tipo)
-        {
-            _tipo = tipo;
-        }
-
+        
         public double GetBonificacao()
         {
-            if (_tipo == 1)
-                return Salario;
-
             return Salario * 0.10;
         }
     }
